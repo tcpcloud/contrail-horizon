b037a118abbf0379a2a630eeab44f026765cfb04
Ales Komarek <mail@newt.cz>
2014-11-23 17:20:30 +0100

    overrides override

--- contrail_openstack_dashboard/overrides.py
+++ contrail_openstack_dashboard/overrides.py
@@ -3,6 +3,49 @@ from django.utils.translation import ugettext_lazy as _
 
 import horizon
 
+
+try:
+    projects_dashboard = horizon.get_dashboard("project")
+    try:
+        topology_panel = projects_dashboard.get_panel("network_topology")
+        projects_dashboard.unregister(topology_panel.__class__)
+    except:
+        pass
+    try:
+        network_panel = projects_dashboard.get_panel("networks")
+        projects_dashboard.unregister(network_panel.__class__)
+    except:
+        pass
+    try:
+        routers_panel = projects_dashboard.get_panel("routers")
+        projects_dashboard.unregister(routers_panel.__class__)
+    except:
+        pass
+    try:
+        lb_panel = projects_dashboard.get_panel("loadbalancers")
+        projects_dashboard.unregister(lb_panel.__class__)
+    except:
+        pass
+except:
+    pass
+
+
+try:
+    admin_dashboard = horizon.get_dashboard("admin")
+    try:
+        admin_net_panel = admin_dashboard.get_panel("networks")
+        admin_dashboard.unregister(admin_net_panel.__class__)
+    except:
+        pass
+    try:
+        admin_router_panel = admin_dashboard.get_panel("routers")
+        admin_dashboard.unregister(admin_router_panel.__class__)
+    except:
+        pass
+except:
+    pass
+
+
 from contrail_openstack_dashboard.openstack_dashboard.dashboards.project.networking.panel \
     import Networking
 from contrail_openstack_dashboard.openstack_dashboard.dashboards.admin.networking.panel \
@@ -51,45 +94,3 @@ class LoadBalancer(horizon.Panel):
     slug = 'lbaas'
     urls = 'contrail_openstack_dashboard.openstack_dashboard.dashboards.admin.lbaas.urls'
 
-
-
-try:
-    projects_dashboard = horizon.get_dashboard("project")
-    try:
-        topology_panel = projects_dashboard.get_panel("network_topology")
-        projects_dashboard.unregister(topology_panel.__class__)
-    except:
-        pass
-    try:
-        network_panel = projects_dashboard.get_panel("networks")
-        projects_dashboard.unregister(network_panel.__class__)
-    except:
-        pass
-    try:
-        routers_panel = projects_dashboard.get_panel("routers")
-        projects_dashboard.unregister(routers_panel.__class__)
-    except:
-        pass
-    try:
-        lb_panel = projects_dashboard.get_panel("loadbalancers")
-        projects_dashboard.unregister(lb_panel.__class__)
-    except:
-        pass
-
-except:
-    pass
-
-try:
-    admin_dashboard = horizon.get_dashboard("admin")
-    try:
-        admin_net_panel = admin_dashboard.get_panel("networks")
-        admin_dashboard.unregister(admin_net_panel.__class__)
-    except:
-        pass
-    try:
-        admin_router_panel = admin_dashboard.get_panel("routers")
-        admin_dashboard.unregister(admin_router_panel.__class__)
-    except:
-        pass
-except:
-    pass
